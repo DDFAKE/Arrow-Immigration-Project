65bbe04d7072014c4536df58d19e1dd39f15b145
Dhairya_Patel <101242478+DDFAKE@users.noreply.github.com>
2025-01-20 22:05:12 -0800

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,24 @@
+# from re import template
+# from unicodedata import name
+# from django import views
+# from django.contrib import admin
+# from django.urls import path
+# from visa import views
+# from django.views.generic import TemplateView
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from .import views
+# urlpatterns = [
+#     path('index/',TemplateView.as_view(template_name="index.html")),
+#     path('main_menu/',views.main_menu),
+#     path('sub_menu/',views.sub_menu),
+#     path('navbar/',views.navbar),
+#     #path('country/',views.country),
+#     #path('coaching/',views.coaching),
+#     path('image/',views.image),
+#     path('visa/',views.visa),
+#     path('contactus/',views.contact_us),
+#     path('work/',views.work),
+# ]
+# urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
